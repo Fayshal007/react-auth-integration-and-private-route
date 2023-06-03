5c7baad5926808ad38920e116d012594e69c0d0d
Fayshal007 <fayshalislam5@gmail.com>
2023-06-03 13:27:43 +0600

    input value to variable done and authprovider star

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+            <div className="hero-content flex-col ">
+                <div className="text-center">
                     <h1 className="text-5xl font-bold">Please Login now!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={handleSubmit}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const AuthContext = createContext(null);
+
+const AuthProviders = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProviders;
